2cc98bc50e7f768d20bb36578054f1bc1244b036
Thomas Frei <thomas.frei@autexis.com>
2019-09-20 22:48:47 +0200

    make it runnable on windows, catch pipins errors

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 const express = require('express');
-const pipins = require('pi-pins');
-
 const player = require('play-sound')(opts = {
   player: 'mpg123',
 });
 
-const button_prev = pipins.connect(17);
-button_prev.mode('in');
-const button_stop = pipins.connect(27);
-button_stop.mode('in');
-const button_play = pipins.connect(22);
-button_play.mode('in');
-const button_next = pipins.connect(26);
-button_next.mode('in');
+try {
+  pipins = require('pi-pins');
+
+  const button_prev = pipins.connect(17);
+  button_prev.mode('in');
+  const button_stop = pipins.connect(27);
+  button_stop.mode('in');
+  const button_play = pipins.connect(22);
+  button_play.mode('in');
+  const button_next = pipins.connect(26);
+  button_next.mode('in');
+
+  button_prev.on('fall', function () {       // rise, fall, both
+    console.log("prev-1");
+    player.play('./assets/1.mp3',() =>{});
+  });
+  button_stop.on('fall', function () {       // rise, fall, both
+    console.log("stop-2");
+    player.play('./assets/2.mp3',() =>{});
+  });
+  button_stop.on('fall', function () {       // rise, fall, both
+    console.log("play-3");
+    player.play('./assets/3.mp3',() =>{});
+  });
+  button_stop.on('fall', function () {       // rise, fall, both
+    console.log("next-4");
+    player.play('./assets/4.mp3',() =>{});
+  });
+
+} catch (err) {
+  console.log('pipins error, probably running NOT on rpi hardware', err);
+}
 
 // Constants
 const PORT = 80;
@@ -44,20 +66,3 @@ app.get('/4', (req, res) => {
 app.listen(PORT, HOST);
 player.play('./assets/radio_bereit.mp3',() =>{});
 console.log(`Running on http://${HOST}:${PORT}`);
-
-button_prev.on('fall', function () {       // rise, fall, both
-  console.log("prev-1");
-  player.play('./assets/1.mp3',() =>{});
-});
-button_stop.on('fall', function () {       // rise, fall, both
-  console.log("stop-2");
-  player.play('./assets/2.mp3',() =>{});
-});
-button_stop.on('fall', function () {       // rise, fall, both
-  console.log("play-3");
-  player.play('./assets/3.mp3',() =>{});
-});
-button_stop.on('fall', function () {       // rise, fall, both
-  console.log("next-4");
-  player.play('./assets/4.mp3',() =>{});
-});
