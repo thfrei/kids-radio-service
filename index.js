8e0ab7453ebd7ed492a75ac326d49396070728b0
Thomas Frei <thomas.frei@autexis.com>
2019-09-20 22:42:43 +0200

    remove omxplayer

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const player = require('play-sound')(opts = {
   player: 'mpg123',
 });
 
-const omx = require('node-omxplayer');
-
 const button_prev = pipins.connect(17);
 button_prev.mode('in');
 const button_stop = pipins.connect(27);
@@ -27,39 +25,39 @@ app.get('/', (req, res) => {
 });
 
 app.get('/1', (req, res) => {
-  player.play('./assets/1.mp3');
+  player.play('./assets/1.mp3',() =>{});
   res.send('1');
 });
 app.get('/2', (req, res) => {
-  player.play('./assets/2.mp3');
+  player.play('./assets/2.mp3',() =>{});
   res.send('2');
 });
 app.get('/3', (req, res) => {
-  player.play('./assets/3.mp3');
+  player.play('./assets/3.mp3',() =>{});
   res.send('3');
 });
 app.get('/4', (req, res) => {
-  player.play('./assets/4.mp3');
+  player.play('./assets/4.mp3',() =>{});
   res.send('4');
 });
 
 app.listen(PORT, HOST);
-player.play('./assets/radio_bereit.mp3');
+player.play('./assets/radio_bereit.mp3',() =>{});
 console.log(`Running on http://${HOST}:${PORT}`);
 
 button_prev.on('fall', function () {       // rise, fall, both
   console.log("prev-1");
-  player.play('./assets/1.mp3');
+  player.play('./assets/1.mp3',() =>{});
 });
 button_stop.on('fall', function () {       // rise, fall, both
   console.log("stop-2");
-  player.play('./assets/2.mp3');
+  player.play('./assets/2.mp3',() =>{});
 });
 button_stop.on('fall', function () {       // rise, fall, both
   console.log("play-3");
-  player.play('./assets/3.mp3');
+  player.play('./assets/3.mp3',() =>{});
 });
 button_stop.on('fall', function () {       // rise, fall, both
   console.log("next-4");
-  player.play('./assets/4.mp3');
+  player.play('./assets/4.mp3',() =>{});
 });
