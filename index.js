5680251d8b1b5db892e13127127522fac9af5f9c
Thomas Frei <thomas.frei@autexis.com>
2019-10-04 23:40:09 +0200

    add RPIPlayer class

--- index.js
+++ index.js
@@ -3,10 +3,36 @@ const player = require('play-sound')(opts = {
   player: 'mpg123',
 });
 const _ = require('lodash');
+
+const STATE = {
+  currentSong: 0,
+  files: [
+    './assets/1.mp3',
+    './assets/2.mp3',
+    './assets/3.mp3',
+    './assets/4.mp3',
+    './assets/free_sample.mp3',
+    './assets/radio_bereit.mp3',
+    './assets/voice.mp3',
+  ]
+};
+
+// GPIOs (Buttons)
 try {
   const wpi = require('node-wiring-pi');
   wpi.setup('gpio');
 
+  /**
+   * listen to Pin
+   * Pins:
+   *  27 - previous
+   *  22 - play
+   *  17 - pause
+   *  26 - next
+   *
+   *  @param nr
+   *  @param <function> cb
+   */
   function listenPin(nr, cb) {
     wpi.pinMode(nr, wpi.INPUT);
     wpi.pullUpDnControl(nr, wpi.PUD_UP);
@@ -26,32 +52,59 @@ try {
     });
   }
 
+  class RPIPlayer {
+    constructor() {
+      this.audio;
+    }
+
+    play() {
+      console.log('RPIPlayer play', STATE);
+      this.audio = player.play(STATE.files[STATE.currentSong], err => console.error);
+    }
+
+    stop() {
+      console.log('RPIPlayer stop', STATE);
+      if (!_.isNil(this.audio)) {
+        this.audio.kill();
+      }
+    }
+
+    previous() {
+      console.log('RPIPlayer previous', STATE);
+      STATE.currentSong = STATE.currentSong === 0 ? 0 : STATE.currentSong--;
+      console.log('RPIPlayer previous', STATE);
+    }
+
+    next() {
+      console.log('RPIPlayer next', STATE);
+      STATE.currentSong = STATE.currentSong === STATE.files.length - 1 ? STATE.files.length - 1 : STATE.currentSong++;
+      console.log('RPIPlayer next', STATE);
+    }
+  }
+  const RPIAudio = new RPIPlayer();
+
+
   listenPin(27, (delta) => {
-    console.log('play asset');
-    player.play('./assets/1.mp3',() =>{});
+    RPIAudio.previous();
   });
   listenPin(22, () => {
-    console.log('play asset');
-    player.play('./assets/2.mp3',() =>{});
+    RPIAudio.stop();
   });
   listenPin(17, () => {
-    console.log('play asset');
-    player.play('./assets/3.mp3',() =>{});
+    RPIAudio.play();
   });
   listenPin(26, () => {
-    console.log('play asset');
-    player.play('./assets/4.mp3',() =>{});
+    RPIAudio.next();
   });
 
 } catch (err) {
   console.log('wiring pi error, probably running NOT on rpi hardware', err);
 }
 
-// Constants
+// Web Server
 const PORT = 80;
 const HOST = '0.0.0.0';
 
-// App
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello world\n');
